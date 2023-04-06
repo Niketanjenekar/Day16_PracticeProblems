8c4405b5b84d8bd50992cc984eac05eb8e71a783
“Niketanjenekar” <niketanjenekar31@gmail.com>
2023-04-06 16:46:43 +0530

    Welcome message

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace BinaryTreeDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Data Structures and Algorithm Problems");
+            
+        }
+    }
+}
