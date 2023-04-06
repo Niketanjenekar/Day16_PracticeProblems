f847a57eb1726d78a14372a21869ad754ccf67fb
“Niketanjenekar” <niketanjenekar31@gmail.com>
2023-04-06 16:55:25 +0530

    Insertion Sorting

--- Program.cs
+++ Program.cs
@@ -6,20 +6,8 @@
         {
             Console.WriteLine("Welcome to Data Structures and Algorithm Problems");
 
-
-
-            Binarytree binaryTree = new Binarytree();
-
-            binaryTree.Add(65);
-            binaryTree.Add(70);
-            binaryTree.Add(60);
-            binaryTree.Add(50);
-            binaryTree.Add(30);
-            binaryTree.Add(85);
-            binaryTree.Add(10);
-            binaryTree.Add(95);
-
-            binaryTree.TravarsePreorder(binaryTree.Root);
+            InsertionSort obj = new InsertionSort();
+            obj.Insertionsort();
 
             
 
