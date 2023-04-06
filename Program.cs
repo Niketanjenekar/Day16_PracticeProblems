df2b153d85b81f7ce668ff9a9689a9fe19af012b
“Niketanjenekar” <niketanjenekar31@gmail.com>
2023-04-06 16:53:25 +0530

    Binary search

--- Program.cs
+++ Program.cs
@@ -6,8 +6,22 @@
         {
             Console.WriteLine("Welcome to Data Structures and Algorithm Problems");
 
-            BubbleSorting.BubbleSort();
 
+
+            Binarytree binaryTree = new Binarytree();
+
+            binaryTree.Add(65);
+            binaryTree.Add(70);
+            binaryTree.Add(60);
+            binaryTree.Add(50);
+            binaryTree.Add(30);
+            binaryTree.Add(85);
+            binaryTree.Add(10);
+            binaryTree.Add(95);
+
+            binaryTree.TravarsePreorder(binaryTree.Root);
+
+            
 
         }
     }
